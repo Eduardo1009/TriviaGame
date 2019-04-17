6934942b239f59fb606b2db1a316ac9c104fc70b
Eduardo Sanchez <eduardosanchez@Eduardos-MacBook-Pro.local>
2019-04-16 23:34:28 -0700

    triviaGame

---
+++ assets/javascript/app.js
@@ -0,0 +1,131 @@
+$("#startGame").on("click", function(event) {
+    $("#startGame").hide();
+    $("#counter").html("<h2> </h2>");
+    $("#trivia").html("<h2>QUESTIONS GO HERE</h2>");
+    clock();
+    generateHTML();
+
+});
+
+$("body").on("click", ".answer", function(event) {
+    event.preventDefault();
+    selectedAnswer = $(this).text();
+    if (selectedAnswer === correctArray[counter]) {
+        clearInterval(time);
+        correctAnswer();
+    } else {
+        clearInterval(time);
+        wrongAnswer();
+    }
+});
+
+$("body").on("click", ".reset-button", function(event) {
+    var correctNum = 0;
+    var incorrectNum = 0;
+    var notAnsweredNum = 0;
+    var counter = 0;
+    var time = 0;
+    var timerCounter = 30;
+    resetGame();
+});
+
+function clock() {
+    time = setInterval(thirty, 1000);
+
+    function thirty() {
+        if (timerCounter === 0) {
+            clearInterval(time);
+            questionTimeout();
+        }
+        if (timerCounter > 0) {
+            timerCounter--;
+        }
+        $(".timer").html(timerCounter);
+    }
+}
+
+function generateHTML() {
+    gameHTML = "<h2 class='text-center timer-p'>Time Remaining: <span class='timer'>" +
+        timerCounter + "</span></h2><h3 class='text-center'>" + questionsArray[counter] +
+        "</h3><h3 class='answer'>A. " + answersArray[counter][0] +
+        "</h3><h3 class='answer'>B. " + answersArray[counter][1] + "</h3><h3 class='answer'>C. " +
+        answersArray[counter][2] + "</h3><h3 class='answer'>D. " + answersArray[counter][3] + "</h3>";
+    $(".gameDiv").html(gameHTML);
+};
+
+function correctAnswer() {
+    correctNum++;
+    gameHTML = "<h2 class='text-center timer-p'>Time Remaining: <span class='timer'>" +
+        timerCounter + "</span></h2><h3 class='text-center'>Correct! The answer is: " +
+        correctArray[counter] + "</h3>";
+    $(".gameDiv").html(gameHTML);
+    setTimeout(wait, 2500);
+}
+
+function wrongAnswer() {
+    incorrectNum++;
+    gameHTML = "<h2 class='text-center timer-p'>Time Remaining: <span class='timer'>" +
+        timerCounter + "</span></h2><h3 class='text-center'>Incorrect! The answer is: " +
+        correctArray[counter] + "</h3>";
+    $(".gameDiv").html(gameHTML);
+    setTimeout(wait, 2500);
+}
+
+function questionTimeout() {
+    notAnsweredNum++;
+    gameHTML = "<h2 class='text-center timer-p'>Time Remaining: <span class='timer'>" +
+        timerCounter + "</span></h2><h3 class='text-center'>You ran out of time!</h3>" +
+        "<h3>The correct answer is: " + correctArray[counter] + "</h3>";
+    $(".gameDiv").html(gameHTML);
+    setTimeout(wait, 2500);
+}
+
+function endOfGame() {
+    gameHTML = "<h2 class='text-center timer-p'>Time Remaining: <span class='timer'>" +
+        timerCounter + "</span></h2><h3 class='text-center'>All done, here's how you did!</h3>" +
+        "<h3 class='summary-correct'>Correct Answers: " + correctNum +
+        "</h3><h3>Wrong Answers: " + incorrectNum + "</h3><h3>Unanswered: " +
+        notAnsweredNum + "</h3>" +
+        "<h3 class='text-center reset-button-container'><a class='btn btn-primary btn-lg btn-block reset-button' href='#' role='button'>Reset The Quiz!</a></h3>";
+    $(".gameDiv").html(gameHTML);
+}
+
+function wait() {
+    if (counter < 1) {
+        counter++;
+        generateHTML();
+        timerCounter = 30;
+        clock();
+    } else {
+        endOfGame();
+    }
+}
+
+function resetGame() {
+    var correctNum = 0;
+    var incorrectNum = 0;
+    var notAnsweredNum = 0;
+    var counter = 0;
+    var time = 0;
+    var timerCounter = 30;
+    clock();
+}
+
+var questionsArray = [
+    "Where is the smallest bone in the body?",
+    "Who cut Van Goghs ear?"
+];
+var answersArray = [
+    ["Toes", "Ears", "Eyebrows", "Spine"],
+    ["His mom", "Jeff from coding bootcamp", "Himself", "A train"]
+];
+var correctArray = [
+    "B. Ears",
+    "C. Himself"
+];
+var correctNum = 0;
+var incorrectNum = 0;
+var notAnsweredNum = 0;
+var counter = 0;
+var time = 0;
+var timerCounter = 30;
